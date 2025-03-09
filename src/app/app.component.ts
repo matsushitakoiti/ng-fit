981e713ac0067e0a8a73831aa255567b9c6343fc
matsushitakoiti <matsushitakoiti@gmail.com>
2025-03-09 12:06:12 -0300

    Initial Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+type WeightEntry = {
+  date: string;
+  weight: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ng-fit';
+  weight = 0;
+
+  
+  // updateWeight(event:Event):void {
+  //   const input = event.target as HTMLInputElement
+  //   this.weight = Number(input.value);
+  // }
+
+  save():void {
+    const data:WeightEntry = {
+      date: "2025.03.09",
+      weight: this.weight
+    }
+    console.log(data)
+  }
 }
